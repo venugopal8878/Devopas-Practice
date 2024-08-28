679205c77a15ff3254f647492f04d44bca1401da
venugopal8878 <mallavallivenu@gmail.com>
2024-08-28 23:42:54 +0530

    add varaible names

--- conversions.sh
+++ conversions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+person1=suresh
+person2=venu
 
-echo "suresh:hello venu how are you"
-echo "venu:I am fine how about u and how is going devops learning"
-echo "suresh: good.linux is completed present learning shll scripting"
-echo "venu:Okay suresh keep"
+echo "$person1:hello ${person2} how are you"
+echo "$person2:I am fine how about u and how is going devops learning"
+echo "$person1: good.linux is completed present learning shll scripting"
+echo "$person2:Okay ${person1} keep"
