39fe6e1ff321618ff014a2af484290cae34afec0
venugopal8878 <mallavallivenu@gmail.com>
2024-08-28 22:59:11 +0530

    add code

---
+++ conversions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "suresh:hello venu how are you"
+echo "venu:I am fine how about u and how is going devops learning"
+echo "suresh: good.linux is completed present learning shll scripting"
+echo "venu:Okay suresh keep"
