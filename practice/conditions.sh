37467b1dac52e8ee74b81477dede7993fc7aa397
venugopal8878 <mallavallivenu@gmail.com>
2024-08-30 16:53:51 +0530

    if conditin

---
+++ practice/conditions.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+number=$0
+
+if [ $number gt 20 ] #gt-greater than eq-equla -nq not qual
+then
+   echo "given number $number greater than 20"
+else
+    echo "not equal to $number 20"
+fi
