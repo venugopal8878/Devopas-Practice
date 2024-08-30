8882dfdde6db5c8279973d746fb19c615851bee5
venugopal8878 <mallavallivenu@gmail.com>
2024-08-30 14:56:35 +0530

    add files to new dir

---
+++ practice/conversions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1:hello ${person2} how are you"
+echo "$person2:I am fine how about u and how is going devops learning"
+echo "$person1: good.linux is completed present learning shll scripting"
+echo "$person2:Okay ${person1} keep"
---
+++ practice/variable.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+echo " enter your user name"
+
+read username
+
+echo "entered user name is:${username}"
+
+echo "enter your password"
+
+read -s password
+
+echo "enter password is:${password}"
