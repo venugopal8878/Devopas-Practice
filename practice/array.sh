ba719ab602e2b5d66e83fe0a69ac32158b3d97c2
venugopal8878 <mallavallivenu@gmail.com>
2024-08-30 15:05:17 +0530

    add array file

---
+++ practice/array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#creating the array
+
+fruits=("apple" "orabge" "promogranet" "water melon") #array
+
+echo "first fruit is ${fruits[0]}"
+echo "secon fruit is ${fruits[1]}"
+echo "Third fruit is ${fruits[2]}"
+echo "fourth fruit is ${fruits[3]}"
