a00d41ebf08d6e6368d64647412d583a24d885b8
venugopal8878 <mallavallivenu@gmail.com>
2024-11-18 22:15:21 +0530

    docker install

---
+++ docker-insatll.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+userid =$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+root_check(){
+    if [ $userid -ne 0]
+    then
+    echo -e "Please run the script with root user $R" 
+    exit
+    fi
+}
+
+VALIDATE(){
+     if [$1 -ne 0]
+ then 
+ echo -e "$1 command failed please recheckonce $R"
+ else
+ echo -e "$G the commnand is success $2"
+ fi
+}
+
+docker --version
+
+If ( $? -ne 0 )
+echo "docker is alredy there $N"
+else
+ echo "going to install docker"
+
+ sudo dnf -y install dnf-plugins-core
+ VALIDATE $? "install docker"
+
+
+ 
+ 
