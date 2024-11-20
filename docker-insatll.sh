7918221b9d39936f50bcd46ebd77883772eee510
venugopal8878 <mallavallivenu@gmail.com>
2024-11-20 10:54:48 +0530

    Update docker-insatll.sh

--- docker-insatll.sh
+++ docker-insatll.sh
@@ -31,7 +31,7 @@ dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 VALIDATE $? "install docker second"
 
-dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
 systemctl enable --now docker
